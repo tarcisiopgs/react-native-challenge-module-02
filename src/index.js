5dbe298450311114eedf950729dd8331cccc86cb
Tarcísio Pedro <tarcisiopgs@gmail.com>
2019-06-04 17:04:04 -0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+import createNavigator from '~/routes';
+import AsyncStorage from '@react-native-community/async-storage';
+
+export default class App extends Component {
+  state = {
+    userChecked: false,
+    userLogged: false,
+  };
+
+  async componentDidMount() {
+    const username = await AsyncStorage.getItem('@Githuber:username');
+
+    this.setState({ userChecked: true, userLogged: !!username });
+  }
+
+  render() {
+    const { userChecked, userLogged } = this.state;
+
+    if (!userChecked) return null;
+
+    const Routes = createNavigator(userLogged);
+
+    return <Routes />;
+  }
+}
